64ea50c94122cd6a8a0721cfbbc3de51d56e282c
ThePotatoArchivist <josiahfu@gmail.com>
2025-01-31 01:42:49 -0800

    Add AlternateCodec

--- src/main/kotlin/archives/tater/rpgskills/data/IngredientCodec.kt
+++ src/main/kotlin/archives/tater/rpgskills/data/IngredientCodec.kt
@@ -3,7 +3,6 @@ package archives.tater.rpgskills.data
 import archives.tater.rpgskills.mixin.data.IngredientAccessor
 import archives.tater.rpgskills.mixin.data.StackEntryAccessor
 import archives.tater.rpgskills.mixin.data.TagEntryAccessor
-import com.mojang.datafixers.util.Either
 import com.mojang.serialization.Codec
 import net.minecraft.recipe.Ingredient
 import net.minecraft.registry.Registries
@@ -11,28 +10,16 @@ import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
 
 @Suppress("CAST_NEVER_SUCCEEDS")
-val INGREDIENT_CODEC: Codec<Ingredient> = Codec.either(
-    TagKey.codec(RegistryKeys.ITEM).xmap(
-        { TagEntryAccessor.newTagEntry(it) },
-        { (it as TagEntryAccessor).tag }
-    ),
+val INGREDIENT_CODEC: Codec<Ingredient> = AlternateCodec(
     Registries.ITEM.codec.xmap(
         { StackEntryAccessor.newStackEntry(it.defaultStack) },
         { (it as StackEntryAccessor).stack.item }
+    ),
+    TagKey.codec(RegistryKeys.ITEM).xmap(
+        { TagEntryAccessor.newTagEntry(it) },
+        { (it as TagEntryAccessor).tag }
     )
-).xmap(
-    { either -> either.map(
-        { it as Ingredient.Entry },
-        { it as Ingredient.Entry }
-    ) },
-    {
-        when (it) {
-            is Ingredient.TagEntry -> Either.left(it)
-            is Ingredient.StackEntry -> Either.right(it)
-            else -> throw AssertionError()
-        }
-    }
-).listOf().xmap(
+) { it is Ingredient.TagEntry }.listOf().xmap(
     { IngredientAccessor.invokeOfEntries(it.stream()) },
     { (it as IngredientAccessor).entries.toList() }
 )
