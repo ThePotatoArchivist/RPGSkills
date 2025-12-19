7ee49640f594f4643ad6af52bb012be0bbbf1e0f
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-18 19:45:07 -0800

    Show `or` for multiple requirents

--- src/main/kotlin/archives/tater/rpgskills/RequirementTooltip.kt
+++ src/main/kotlin/archives/tater/rpgskills/RequirementTooltip.kt
@@ -13,21 +13,22 @@ import net.minecraft.item.ItemStack
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
+import net.minecraft.text.Texts
 import net.minecraft.util.Formatting
 import java.util.function.Consumer
 import kotlin.collections.component1
 import kotlin.collections.component2
 
 object RequirementTooltip {
-    val REQUIRES = Translation.unit("$MOD_ID.tooltip.stack.requires")
-    val REQUIRES_ANY = Translation.unit("$MOD_ID.tooltip.stack.requires.any")
     val REQUIREMENT = Translation.arg("$MOD_ID.tooltip.stack.requirement") { formatted(Formatting.DARK_GRAY) }
     val HINT = Translation.unit("$MOD_ID.tooltip.stack.hint") {
         formatted(Formatting.DARK_GRAY)
     }
-    val USE = Translation.unit("$MOD_ID.tooltip.stack.use")
-    val PLACE = Translation.unit("$MOD_ID.tooltip.stack.place")
-    val CRAFT = Translation.unit("$MOD_ID.tooltip.stack.craft")
+    val REQUIRES = Translation.unit("$MOD_ID.tooltip.stack.requires")
+    val USE_REQUIRES = Translation.unit("$MOD_ID.tooltip.stack.requires.use")
+    val PLACE_REQUIRES = Translation.unit("$MOD_ID.tooltip.stack.requires.place")
+    val CRAFT_REQUIRES = Translation.unit("$MOD_ID.tooltip.stack.requires.craft")
+    val OR = Translation.unit("$MOD_ID.tooltip.stack.requires.or")
 
     @JvmStatic
     fun getRequirement(requirement: Collection<Map.Entry<RegistryEntry<Skill>, Int>>, player: PlayerEntity, tooltip: Boolean = true): MutableText {
@@ -48,10 +49,8 @@ object RequirementTooltip {
         }
     }
 
-    fun getTitle(lockGroup: LockGroup, player: PlayerEntity, prefix: Text? = null, tooltip: Boolean = true): Text =
-        (if (lockGroup.requirements.size == 1) REQUIRES.text() else REQUIRES_ANY.text()).let {
-            if (prefix == null) it else ((prefix as? MutableText) ?: prefix.copy()).append(it)
-        }.apply {
+    fun getTitle(lockGroup: LockGroup, player: PlayerEntity, title: MutableText?, tooltip: Boolean = true): Text =
+        (title ?: REQUIRES.text()).apply {
             formatted(
                 when {
                     !tooltip -> Formatting.BLACK
@@ -63,14 +62,17 @@ object RequirementTooltip {
 
     @JvmStatic
     @JvmOverloads
-    fun appendRequirements(lockGroup: LockGroup, player: PlayerEntity, text: Consumer<Text>, prefix: Text? = null, tooltip: Boolean = true) {
-        text.accept(getTitle(lockGroup, player, prefix, tooltip))
-        for (requirement in lockGroup.requirements)
-            text.accept(REQUIREMENT.text(getRequirement(requirement.entries, player, tooltip)))
+    fun appendRequirements(lockGroup: LockGroup, player: PlayerEntity, text: Consumer<Text>, title: MutableText? = null, tooltip: Boolean = true) {
+        text.accept(getTitle(lockGroup, player, title, tooltip))
+        lockGroup.requirements.forEachIndexed { index, requirement ->
+            text.accept(REQUIREMENT.text(getRequirement(requirement.entries, player, tooltip)).apply {
+                if (index + 1 < lockGroup.requirements.size) append(OR.text)
+            })
+        }
     }
 
-    fun appendRequirements(lockGroup: LockGroup, player: PlayerEntity, text: MutableList<Text>, prefix: Text? = null, tooltip: Boolean = true) {
-        appendRequirements(lockGroup, player, text::add, prefix, tooltip)
+    fun appendRequirements(lockGroup: LockGroup, player: PlayerEntity, text: MutableList<Text>, title: MutableText? = null, tooltip: Boolean = true) {
+        appendRequirements(lockGroup, player, text::add, title, tooltip)
     }
 
     @JvmStatic
@@ -81,13 +83,22 @@ object RequirementTooltip {
 
         if (useLock == null && placeLock == null && craftLock == null) return
 
-        if (!keyPressed) {
-            tooltip.add(HINT.text)
-            return
-        }
+//        if (!keyPressed) {
+//            tooltip.add(HINT.text)
+//            return
+//        }
 
-        useLock?.let { appendRequirements(it, player, tooltip, USE.text) }
-        placeLock?.let { appendRequirements(it, player, tooltip, PLACE.text) }
-        craftLock?.let { appendRequirements(it, player, tooltip, CRAFT.text) }
+        useLock?.let {
+            tooltip.add(Text.empty())
+            appendRequirements(it, player, tooltip, USE_REQUIRES.text())
+        }
+        placeLock?.let {
+            tooltip.add(Text.empty())
+            appendRequirements(it, player, tooltip, PLACE_REQUIRES.text())
+        }
+        craftLock?.let {
+            tooltip.add(Text.empty())
+            appendRequirements(it, player, tooltip, CRAFT_REQUIRES.text())
+        }
     }
 }
