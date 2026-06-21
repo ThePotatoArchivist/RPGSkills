663579f85883b9a63420c75117c861d716027db3
ThePotatoArchivist <josiahfu@gmail.com>
2026-06-20 17:34:10 -0700

    Fix connector crash

--- src/main/java/archives/tater/rpgskills/mixin/advancement/freshcraft/ItemEntityMixin.java
+++ src/main/java/archives/tater/rpgskills/mixin/advancement/freshcraft/ItemEntityMixin.java
@@ -3,20 +3,41 @@ package archives.tater.rpgskills.mixin.advancement.freshcraft;
 import archives.tater.rpgskills.cca.FreshCraftComponent;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 @Mixin(ItemEntity.class)
-public class ItemEntityMixin {
+public abstract class ItemEntityMixin extends Entity {
+    public ItemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Shadow
+    public abstract ItemStack getStack();
+
     @Inject(
-            method = "merge(Lnet/minecraft/entity/ItemEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/ItemEntity;Lnet/minecraft/item/ItemStack;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ItemEntity;discard()V")
+            method = "tryMerge(Lnet/minecraft/entity/ItemEntity;)V",
+            at = @At("TAIL")
     )
-    private static void mergeFreshCount(ItemEntity targetEntity, ItemStack targetStack, ItemEntity sourceEntity, ItemStack sourceStack, CallbackInfo ci) {
-        targetEntity.getComponent(FreshCraftComponent.KEY).addFreshCount(sourceEntity.getComponent(FreshCraftComponent.KEY).getFreshCount());
+    private void mergeFreshCount(ItemEntity other, CallbackInfo ci) {
+        Entity source;
+        Entity target;
+        if (getStack().isEmpty()) {
+            source = this;
+            target = other;
+        } else if (other.getStack().isEmpty()) {
+            source = other;
+            target= this;
+        } else return;
+
+        target.getComponent(FreshCraftComponent.KEY).addFreshCount(source.getComponent(FreshCraftComponent.KEY).getFreshCount());
     }
 }
